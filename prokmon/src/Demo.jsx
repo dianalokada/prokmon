7e27c6843fc0b7a8431e63aeb9e6a2e616759a08
Diana <dianaalokhina@gmail.com>
2023-12-26 19:47:49 -0500

    create Demo and Meet the team page

--- prokmon/src/Demo.jsx
+++ prokmon/src/Demo.jsx
@@ -1,12 +1,13 @@
-import { Container, Typography, Grid, Stack } from '@mui/material'
+import { Container, Typography, Grid, Stack, Card, CardMedia, CardContent } from '@mui/material'
 
 function Demo() {
   
     return (
         <Container>
-            <Typography variant='h5' component='h1' sx={{textAlign: 'center', marginTop: '30px', marginBottom: '30px', fontWeight: 'bold', fontSize: '50px'}}> See KMon in Action! </Typography>
+            <Typography variant='h5' component='h1' sx={{textAlign: 'center', marginTop: '30px', marginBottom: '30px', fontWeight: 'bold', fontSize: '50px'}}> See KMon in Action </Typography>
             <Grid container spacing={2} alignItems="center" justifyContent="center">
 
+                {/* first grid */}
                 <Grid item xs={6} sx={{ textAlign: 'center' }}> 
 
                     <Stack spacing={2}>
@@ -15,8 +16,17 @@ function Demo() {
 
                 </Grid>
 
+                {/* second grid */}
                 <Grid item xs={6} sx={{ textAlign: 'center' }}>
-                    
+                    <Card>
+                        <CardMedia component="video" controls>
+                            <source src="path_to_your_video.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </CardMedia>
+                        <CardContent>
+                            {/* Additional content like title or description */}
+                        </CardContent>
+                    </Card>
                 </Grid>
 
             </Grid>
--- prokmon/src/Team.jsx
+++ prokmon/src/Team.jsx
@@ -1,10 +1,248 @@
-import { Container, Typography } from '@mui/material'
+import { Container, Typography,  Grid, Card, Box, IconButton } from '@mui/material'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import LucyImage from './assets/Lucy.png';
+import HudsonImage from './assets/Hudson.png';
+import ElliotImage from './assets/Elliot.png';
+import DarrylImage from './assets/Darryl.png';
+import DianaImage from './assets/Diana.png';
 
 function Team() {
   
     return (
         <Container>
             <Typography variant='h5' component='h1' sx={{textAlign: 'center', marginTop: '30px', marginBottom: '30px', fontWeight: 'bold', fontSize: '50px'}}> Meet the Team </Typography>
+            
+            <Container sx={{
+                //backgroundColor: '#1f1f2b', // Replace with the hexadecimal code of the color you like
+                paddingY: '80px', // Add some vertical padding
+                //borderRadius: '8px', // Optional: if you want rounded corners
+                textAlign: 'center', // Centers the text horizontally
+                marginTop: '30px',
+                marginBottom: '30px',
+                maxWidth: "md"
+            }}>
+                <Grid container spacing={4} justifyContent="center" >
+
+
+
+
+                    {/* First person */}
+                    <Grid item xs={12} sm={4}>
+                        <Card sx={{ 
+                            border: '1px solid #ffffff33', 
+                            backgroundColor: '#101418', 
+                            borderRadius: '20px', 
+                            color: 'white', 
+                            boxShadow: '0px 0px 14px #bb99cc',
+                            padding: '2rem', 
+                            minWidth: '150px', 
+                            minHeight: '120px',
+                            overflow: 'hidden',
+                            }}
+                        >
+                            <img 
+                                src={LucyImage} 
+                                alt="pic" 
+                                style = {{
+                                    width: '100%', 
+                                    height: 'auto',
+                                    borderRadius: '5px'
+                                }} 
+                            />
+                            <Box sx = {{
+                                paddingY: '20px', 
+                                paddingX: '16px', 
+                                textAlign: 'center', 
+                                }}
+                                >
+                                <Typography variant='h6' component='h2' gutterBottom >
+                                    Lucy Wu
+                                </Typography>
+                                
+                                <Box sx = {{display: 'flex', justifyContent: 'center'}}>
+                                    <IconButton component="a" href="https://github.com/lwu15" target="_blank" rel="noopener noreferrer" disableRipple disableElevation sx={{ padding: '0px', margin: '0 3px' }} ><GitHubIcon sx={{ width: '30px', height: '30px' }} /></IconButton>
+                                    <IconButton component="a" href="https://www.linkedin.com/in/lwu15/" target="_blank" rel="noopener noreferrer" disableRipple disableElevation sx={{ padding: '0px', margin: '0 3px' }} ><LinkedInIcon sx={{ width: '30px', height: '30px' }} /></IconButton>
+                                </Box>
+                                </Box>
+                        </Card>
+                    </Grid>
+                    {/* end of first person */}
+
+
+
+
+
+                    {/* Second person */}
+                    <Grid item xs={12} sm={4}>
+                        <Card sx={{ 
+                            border: '1px solid #ffffff33', 
+                            backgroundColor: '#101418', 
+                            borderRadius: '20px', 
+                            color: 'white', 
+                            boxShadow: '0px 0px 14px #bb99cc',
+                            padding: '2rem', 
+                            minWidth: '150px', 
+                            minHeight: '120px',
+                            overflow: 'hidden',
+                            }}
+                        >
+                            <img 
+                                src={HudsonImage} 
+                                alt="pic" 
+                                style = {{
+                                    width: '100%', 
+                                    height: 'auto',
+                                    borderRadius: '5px'
+                                }} 
+                            />
+                            <Box sx = {{
+                                paddingY: '20px', 
+                                paddingX: '16px', 
+                                textAlign: 'center', 
+                                }}
+                                >
+                                <Typography variant='h6' component='h2' gutterBottom >
+                                    Hudson McLane
+                                </Typography>
+                                
+                                <Box sx = {{display: 'flex', justifyContent: 'center'}}>
+                                    <IconButton component="a" href="https://github.com/redferret1" target="_blank" rel="noopener noreferrer" disableRipple disableElevation sx={{ padding: '0px', margin: '0 3px' }} ><GitHubIcon sx={{ width: '30px', height: '30px' }} /></IconButton>
+                                    {/* <IconButton component="a" href="" target="_blank" rel="noopener noreferrer" disableRipple disableElevation sx={{ padding: '0px', margin: '0 3px' }} ><LinkedInIcon sx={{ width: '30px', height: '30px' }} /></IconButton> */}
+                                </Box>
+                                </Box>
+                        </Card>
+                    </Grid>
+                    {/* end of second person */}
+
+                    {/* start of third person */}
+                    <Grid item xs={12} sm={4}>
+                        <Card sx={{ 
+                            border: '1px solid #ffffff33', 
+                            backgroundColor: '#101418', 
+                            borderRadius: '20px', 
+                            color: 'white', 
+                            boxShadow: '0px 0px 14px #bb99cc',
+                            padding: '2rem', 
+                            minWidth: '150px', 
+                            minHeight: '120px',
+                            overflow: 'hidden',
+                            }}
+                        >
+                            <img 
+                                src={ElliotImage} 
+                                alt="pic" 
+                                style = {{
+                                    width: '100%', 
+                                    height: 'auto',
+                                    borderRadius: '5px'
+                                }} 
+                            />
+                            <Box sx = {{
+                                paddingY: '20px',
+                                paddingX: '16px', 
+                                textAlign: 'center', 
+                                }}
+                                >
+                                <Typography variant='h6' component='h2' gutterBottom >
+                                    Elliot Cho
+                                </Typography>
+                                
+                                <Box sx = {{display: 'flex', justifyContent: 'center'}}>
+                                    <IconButton component="a" href="https://github.com/echochs1" target="_blank" rel="noopener noreferrer" disableRipple disableElevation sx={{ padding: '0px', margin: '0 3px' }} ><GitHubIcon sx={{ width: '30px', height: '30px' }} /></IconButton>
+                                    <IconButton component="a" href="https://www.linkedin.com/in/elliot-hs-cho/" target="_blank" rel="noopener noreferrer" disableRipple disableElevation sx={{ padding: '0px', margin: '0 3px' }} ><LinkedInIcon sx={{ width: '30px', height: '30px' }} /></IconButton>
+                                </Box>
+                                </Box>
+                        </Card>
+                    </Grid>
+                    {/* end of third person */}
+
+                    {/* start of fourth person */}
+                    <Grid item xs={12} sm={4}>
+                        <Card sx={{ 
+                            border: '1px solid #ffffff33', 
+                            backgroundColor: '#101418', 
+                            borderRadius: '20px', 
+                            color: 'white', 
+                            boxShadow: '0px 0px 14px #bb99cc',
+                            padding: '2rem', 
+                            minWidth: '150px', 
+                            minHeight: '120px',
+                            overflow: 'hidden',
+                            }}
+                        >
+                            <img 
+                                src={DarrylImage} 
+                                alt="pic" 
+                                style = {{
+                                    width: '100%', 
+                                    height: 'auto',
+                                    borderRadius: '5px'
+                                }} 
+                            />
+                            <Box sx = {{
+                                paddingY: '20px', 
+                                paddingX: '16px', 
+                                textAlign: 'center', 
+                                }}
+                                >
+                                <Typography variant='h6' component='h2' gutterBottom >
+                                    Darryl Joo
+                                </Typography>
+                                
+                                <Box sx = {{display: 'flex', justifyContent: 'center'}}>
+                                    <IconButton component="a" href="https://github.com/hjdjoo" target="_blank" rel="noopener noreferrer" disableRipple disableElevation sx={{ padding: '0px', margin: '0 3px' }} ><GitHubIcon sx={{ width: '30px', height: '30px' }} /></IconButton>
+                                    {/* <IconButton component="a" href="" target="_blank" rel="noopener noreferrer" disableRipple disableElevation sx={{ padding: '0px', margin: '0 3px' }} ><LinkedInIcon sx={{ width: '30px', height: '30px' }} /></IconButton> */}
+                                </Box>
+                                </Box>
+                        </Card>
+                    </Grid>
+                    {/* end of fourth person */}
+
+                    {/* start of fifth person */}
+                    <Grid item xs={12} sm={4}>
+                        <Card sx={{ 
+                            border: '1px solid #ffffff33', 
+                            backgroundColor: '#101418', 
+                            borderRadius: '20px', 
+                            color: 'white', 
+                            boxShadow: '0px 0px 14px #bb99cc',
+                            padding: '2rem', 
+                            minWidth: '150px', 
+                            minHeight: '120px',
+                            overflow: 'hidden',
+                            }}
+                        >
+                            <img 
+                                src={DianaImage} 
+                                alt="pic" 
+                                style = {{
+                                    width: '100%', 
+                                    height: 'auto',
+                                    borderRadius: '5px'
+                                }} 
+                            />
+                            <Box sx = {{
+                                paddingY: '20px', 
+                                paddingX: '16px', 
+                                textAlign: 'center', 
+                                }}
+                                >
+                                <Typography variant='h6' component='h2' gutterBottom >
+                                    Diana Alokhina
+                                </Typography>
+                                
+                                <Box sx = {{display: 'flex', justifyContent: 'center'}}>
+                                    <IconButton component="a" href="https://github.com/dianalokada" target="_blank" rel="noopener noreferrer" disableRipple disableElevation sx={{ padding: '0px', margin: '0 3px' }} ><GitHubIcon sx={{ width: '30px', height: '30px' }} /></IconButton>
+                                    <IconButton component="a" href="https://www.linkedin.com/in/diana-alokhina/" target="_blank" rel="noopener noreferrer" disableRipple disableElevation sx={{ padding: '0px', margin: '0 3px' }} ><LinkedInIcon sx={{ width: '30px', height: '30px' }} /></IconButton>
+                                </Box>
+                                </Box>
+                        </Card>
+                    </Grid>
+                    {/* end of fifth person */}
+
+                </Grid>
+            </Container>
         </ Container>
     )
 }
